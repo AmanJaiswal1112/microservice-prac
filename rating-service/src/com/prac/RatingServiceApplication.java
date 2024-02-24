93766fb0bbb0612b3209afa3df767ef46211c444
sushant6985 <sushant.kumar239@gmail.com>
2024-02-24 16:22:07 +0530

    Adding rating service

---
+++ rating-service/src/com/prac/RatingServiceApplication.java
@@ -0,0 +1,11 @@
+package com.prac;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RatingServiceApplication.class, args);
+		System.out.println("Hello java");
+	}
+}
---
+++ rating-service/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module com.prac.rating.service {
+}
